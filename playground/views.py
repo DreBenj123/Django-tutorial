af87697700605eee27e7345f79234bf1c57b019e
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2023-03-24 07:26:37 -0400

    Retreiving products

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    for product in query_set:
-        print(product)
+
+    exists = Product.objects.filter(pk=0).exists()
+
     return render(request, 'hello.html', {"name": "Dre"})
