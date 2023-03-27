81b85f9ddce6a07f47612ac9692601671880da08
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2023-03-27 12:19:53 -0400

    Doing querys using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
+from store.models import Customer
+from store.models import Order
 
 
 def say_hello(request):
-
-    exists = Product.objects.filter(pk=0).exists()
-
-    return render(request, 'hello.html', {"name": "Dre"})
+    queryset = Product.objects.filter(
+        Q(inventory__lt=10) | Q(unit_price__lt=20))
+    return render(request, 'hello.html', {"name": "Dre", "products": list(queryset)})
