337fb97dd494cb79253caf5597ae78d5bf590144
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2023-03-28 12:10:06 -0400

    more practice with selecting query fields

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
-from store.models import Product
-from store.models import Customer
-from store.models import Order
+from django.db.models import Q, F
+from store.models import Product, Customer, Order, OrderItem
 
 
 def say_hello(request):
     queryset = Product.objects.filter(
-        Q(inventory__lt=10) | Q(unit_price__lt=20))
+        id__in=OrderItem.objects.values('product_id').distinct()).order_by("title")
+
     return render(request, 'hello.html', {"name": "Dre", "products": list(queryset)})
